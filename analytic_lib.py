98283fbf803fdf50f492812b9b169f4b8ae5d892
Ting Yau <tingyau95@gmail.com>
2019-05-12 18:27:03 +0800

    RSI_Analysis in progress

--- analytic_lib.py
+++ analytic_lib.py
@@ -14,25 +14,53 @@ from talib import MA_Type
 from tools.reader import read_file
 from tools.functionality import np_shift
 
+def RSI__IsOverbought(rsi_value, upper_limit):
+	'''
+	parameter: rsi_value = value from talib.RSI
+	return	 : range of days where overbought happens. (True = 'Overbought')
+	'''
+	dummy_val = 50
+	return np_shift((rsi_value>=upper_limit),1,0)& \
+		  (np_shift(rsi_value,1,dummy_val)>rsi_value)
+		  
+def RSI__IsOversold(rsi_value, lower_limit):
+	'''
+	parameter: rsi_value = value from talib.RSI
+	return	 : range of days where oversold happens. (True = 'Oversold')
+	'''
+	dummy_val = 50
+	return np_shift((rsi_value<=lower_limit),1,0)& \
+		   (np_shift(rsi_value,1,dummy_val)<rsi_value)
 
-def RSI_analysis(close_data):
+def RSI_analysis(close_data, timeperiod, upper_limit = 70, lower_limit = 30):
 	'''
-	parameter: close_data = numpy array closing price (type: np.ndarray.float64)
-	return	 : rsi_buy_signal = numpy array of 1s' and 0s'. 1 indicate buy
-			   rsi_sell signal= numpy array of 1s' and 0s'. 1 indicate sell
-		
+	parameter: close_data = closing price array (type: np.ndarray.float64)
+	return	 : rsi_buy_signal  = range of days to 'Buy'. True = 'Buy'
+			   rsi_sell_signal = range of days to 'Sell'. True ='Sell'	
 	'''
-	rsi_value = talib.RSI(close_data, timeperiod = 16).astype(np.float32)
-	rsi_sell_signal = np.where(np_shift((rsi_value>=80),1)&(np_shift(rsi_value,1)>rsi_value),1,0)
-	rsi_buy_signal = np.where((rsi_value<=20)&(np_shift(rsi_value,1)<rsi_value),1,0)
-	return (rsi_buy_signal,rsi_sell_signal)
-					 
+	dummy_value 	= 50
+	rsi_value		= talib.RSI(close_data, timeperiod).astype(np.float32)
+	rsi_value 		= rsi_value[~np.isnan(rsi_value)] #only select non-nan RSI value			  
+	rsi_buy_signal  = RSI__IsOversold(rsi_value, lower_limit)
+	rsi_sell_signal	= RSI__IsOverbought(rsi_value,upper_limit)
+	return (rsi_value,rsi_buy_signal,rsi_sell_signal)
+	
 if __name__ == "__main__":
 	df = read_file('Econpile_data.csv')
 	Econpile_close = (df['Close'].to_numpy())
-	rsi_buy,rsi_sell = RSI_analysis(Econpile_close)
-	print(rsi_buy[:50])
-	print(rsi_sell[:50])
+	timeperiod = 14
+	rsi_value,rsi_buy,rsi_sell = RSI_analysis(Econpile_close,timeperiod)
+	x_normal = np.arange(0, len(Econpile_close)-timeperiod)
+	y_normal = Econpile_close[timeperiod:]
+	plt.plot(x_normal, y_normal, 'g-')
+	plt.plot(x_normal[rsi_sell], y_normal[rsi_sell],'r.')
+	plt.plot(x_normal[rsi_buy], y_normal[rsi_buy],'b.')
+	
+	# plt.plot(x_normal, rsi_value, 'g-')
+	# plt.plot(x_normal[rsi_sell], rsi_value[rsi_sell],'r.')
+	# plt.plot(x_normal[rsi_buy], rsi_value[rsi_buy],'b.')
+	print(rsi_buy[:150])
+	plt.show()
 	#plot_signal('Econpile',Econpile_close,rsi_buy,'r-')
 	#print(Econpile_close)
 	# print(type(close_price))
