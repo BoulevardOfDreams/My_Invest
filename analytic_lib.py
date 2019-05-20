d7f7cfde5f150fce4fde13948b1839a5b2b2c6b1
Ting Yau <tingyau95@gmail.com>
2019-05-20 23:20:57 +0800

    construct MACD class (in progress)

--- analytic_lib.py
+++ analytic_lib.py
@@ -1,5 +1,6 @@
 # Standard library imports
 import os
+import logging as log
 
 # Third party imports
 import talib
@@ -13,6 +14,22 @@ from talib import MA_Type
 # Local application imports
 from tools.reader import read_file
 from tools.functionality import np_shift
+
+class context():
+	def __init__(self):
+		self.MA200 = 200
+		self.MA100 = 100
+		self.MA50  = 50
+
+def init_logger(log_level):
+
+	log.basicConfig(level = log_level,
+					filename='app.log',
+					filemode='w', 
+					format='[%(levelname)s] - %(threadName)s - %(message)s')
+	log.info('Logging Start')
+	
+	
 
 def RSI__IsOverbought(rsi_value, upper_limit):
 	'''
@@ -45,22 +62,106 @@ def RSI_analysis(close_data, timeperiod, upper_limit = 70, lower_limit = 30):
 	rsi_sell_signal	= RSI__IsOverbought(rsi_value,upper_limit)
 	return (rsi_value,rsi_buy_signal,rsi_sell_signal)
 	
+def SMA_analysis(close_data, period):
+	'''
+	parameter: close_data = closing price array (type: np.ndarray.float64)
+	return	 : 1. signal
+			      - within period  = False(Invalid)
+			      - greater period = return True(above SMA) and False(below SMA)
+			   2. SMA
+				  - moving average result
+	'''
+	data_len 		= len(close_data)
+	SMA				= np.zeros(data_len)
+	signal 			= np.zeros(data_len, dtype = bool)
+	
+	if data_len > period:
+		SMA  			= talib.SMA(close_data, period).astype(np.float32)
+		signal[period:] = close_data[period:] > SMA[period:]
+		log.info('SMA: Complete')
+		return (signal, SMA)
+	else:
+		log.error('SMA: Data length < 200 ')
+		return (signal, SMA)
+
+class MACD():
+	def __init__(self, close_data,\
+				fastperiod  = 12, \
+				shortperiod = 26, \
+				signalperiod= 9      ):
+		
+		self.close_data		= close_data
+		self.fastperiod		= fastperiod
+		self.shortperiod	= shortperiod
+		self.signalperiod	= signalperiod
+		
+		self.macd, self.signal, self.hist = talib.MACD(
+											self.close_data,\
+											self.fastperiod,\
+											self.shortperiod,\
+											self.signalperiod)
+		
+	def analyse(self):
+		log.INFO('MACD	   :Start Anaysis /n 	    \
+				  Parameter:/n						\
+				  fastperiod   = {self.fastperiod  }\
+				  shortperiod  = {self.shortperiod }\
+				  signalperiod = {self.signalperiod}'.format(self=self))
+				  
+		self.macd, self.signal, self.hist = talib.MACD(
+											self.close_data,\
+											self.fastperiod,\
+											self.shortperiod,\
+											self.signalperiod)
+		return (self.macd, self.signal, self.hist)
+		
+	
 if __name__ == "__main__":
+	
+	init_logger(log_level = log.INFO)
 	df = read_file('Econpile_data.csv')
 	Econpile_close = (df['Close'].to_numpy())
-	timeperiod = 14
-	rsi_value,rsi_buy,rsi_sell = RSI_analysis(Econpile_close,timeperiod)
-	x_normal = np.arange(0, len(Econpile_close)-timeperiod)
-	y_normal = Econpile_close[timeperiod:]
-	plt.plot(x_normal, y_normal, 'g-')
-	plt.plot(x_normal[rsi_sell], y_normal[rsi_sell],'r.')
-	plt.plot(x_normal[rsi_buy], y_normal[rsi_buy],'b.')
+	macd = MACD(Econpile_close)
+	# signal, SMA200 = SMA200_analysis(Econpile_close)
+	
+	# x_normal = np.arange(0, len(Econpile_close))
+	# y_normal = Econpile_close
+	# f, axarr = plt.subplots(2, sharex = True)
+	# axarr[0].plot(x_normal, y_normal)
+	# axarr[0].plot(x_normal, SMA200, 'r-')
+	# axarr[1].plot(x_normal, signal, 'g-')
+	
+	# axarr[0].set(ylabel = 'Econpile_data')
+	# axarr[1].set(ylabel = 'Buy Signal'	 )
+	# plt.show()
+	# OPEN LATER 1
+	# timeperiod = 14
+	# rsi_value,rsi_buy,rsi_sell = RSI_analysis(Econpile_close,timeperiod)
+	# x_normal = np.arange(0, len(Econpile_close)-timeperiod)
+	# y_normal = Econpile_close[timeperiod:]
+	# plt.plot(x_normal, y_normal, 'g-')
+	# plt.plot(x_normal[rsi_sell], y_normal[rsi_sell],'r.')
+	# plt.plot(x_normal[rsi_buy], y_normal[rsi_buy],'b.')
+	# print(rsi_buy[:150])
+	# plt.show()
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	# plt.plot(x_normal, rsi_value, 'g-')
 	# plt.plot(x_normal[rsi_sell], rsi_value[rsi_sell],'r.')
 	# plt.plot(x_normal[rsi_buy], rsi_value[rsi_buy],'b.')
-	print(rsi_buy[:150])
-	plt.show()
 	#plot_signal('Econpile',Econpile_close,rsi_buy,'r-')
 	#print(Econpile_close)
 	# print(type(close_price))
