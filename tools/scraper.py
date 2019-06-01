8930d95b29d18addc0e72ebb46e28bd94cbc8fe6
Ting Yau <tingyau95@gmail.com>
2019-06-02 02:05:10 +0800

    scraper v1.0

--- tools/scraper.py
+++ tools/scraper.py
@@ -1,5 +1,6 @@
 # Standard library imports
-import os
+from os import getcwd, listdir, remove
+from os.path import join, isfile
 import logging as log
 
 # Third party imports
@@ -7,6 +8,12 @@ import pandas as pd
 from pandas import ExcelWriter
 from pandas import ExcelFile
 from selenium import webdriver
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+from time import sleep
 
 # Local application imports
 from environment.env_setting import ENVIRONMENT, Host 
@@ -16,6 +23,7 @@ class stock_manager():
         self.url_dict = \
         {'econpile' : 'https://www.investing.com/equities/econpile-holdings-bhd-historical-data',\
          'gamuda  ' : 'https://www.investing.com/equities/gamuda-bhd-historical-data'           }   
+        log.info('Initialize Stock Library')
         
     def list_all(self):
         index = 1
@@ -27,9 +35,9 @@ class stock_manager():
 class csv_scraper():
     
     #directories
-    current_dir       = os.getcwd()
-    database_dir      = os.path.join(current_dir, '..', 'Database')
-    gecko_path        = os.path.join(current_dir, '..', 'tools\gecko\gecko.exe')
+    current_dir       = getcwd()
+    database_dir      = join(current_dir, '..', 'Database')
+    gecko_path        = join(current_dir, '..', 'tools\gecko\gecko.exe')
     
     #firefox configs
     save_config       = 'browser.download.folderList'
@@ -42,7 +50,17 @@ class csv_scraper():
     csv_format        = 'text/csv' #MIME format
     
     def __init__(self, url_dict):
-        self.url_dict = url_dict 
+    
+        self.url_dict    = url_dict
+        self.profile  = 0
+        self.browser  = 0
+        
+        self.setup_firefox()
+        self.login()
+        self.update_all_csv()
+        
+        
+    def setup_firefox(self):
         self.profile  = webdriver.FirefoxProfile()
         self.profile.set_preference(self.save_config      , self.custom_save )
         self.profile.set_preference(self.download_dir     , self.database_dir)
@@ -58,29 +76,57 @@ class csv_scraper():
             self.profile.set_preference(proxy_port       , company_port)
         
         self.browser = webdriver.Firefox(self.profile, executable_path = self.gecko_path)
-        self.login()
         
     def login(self):
-        
-        web_sign_in    = "//a[@class='login bold']"
-        pop_up_sign_in = "//a[@class='newButton orange']"
+     
         url            = "https://www.investing.com/"
-        
+        email	   	   = "emailautomation95@gmail.com"
+        password	   = "Outmyhouse95"
+        webSignIn      = "//a[@class='login bold']"
+        popUpSignIn    = "//a[starts-with(@onclick, 'login')]"
+
+
         self.browser.get(url)
-        self.browser.find_element_by_xpath(web_sign_in).click()
+        self.browser.find_element_by_xpath(webSignIn).click()
+        self.browser.find_element_by_id("loginFormUser_email").send_keys(email)
+        self.browser.find_element_by_id("loginForm_password").send_keys(password)
+        self.browser.find_element_by_xpath(popUpSignIn).click()
         
     def update_all_csv(self):
         
         download_xp = "//a[@title='Download Data']"
+        startDate   = "06/01/2013"
+        endDate     = "06/01/2019"
         
         for url in self.url_dict.values():
             self.browser.get(url)
+            self.browser.find_element_by_id("widgetFieldDateRange").click()
+            
+            Date_txt = self.browser.find_element_by_id("startDate")
+            Date_txt.clear()
+            Date_txt.send_keys(startDate)
+            
+            Date_txt = self.browser.find_element_by_id("endDate")
+            Date_txt.clear()
+            Date_txt.send_keys(endDate)
+            
+            self.browser.find_element_by_id("applyBtn").click()
+            WebDriverWait(self.browser, 5).until(EC.presence_of_element_located((By.ID, "curr_table")))
             self.browser.find_element_by_xpath(download_xp).click()
+            
+    def delete_all_csv(self):
+    
+        all_csv = [csv for csv in listdir(self.database_dir)\
+                   if isfile(join(self.database_dir, csv)) ]
+                   
+        for csv in all_csv:
+            remove(join(self.database_dir, csv))
+        
+        
         
 if __name__ == "__main__":
     stock = stock_manager()
     stock.list_all()
     scraper = csv_scraper(stock.url_dict)
-    
     #search_engine = data_processor()
     
